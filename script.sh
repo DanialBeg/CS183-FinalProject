74eeaef575b0165875556283430481a390a7f6d7
swastyak <43711083+swastyak@users.noreply.github.com>
2021-11-26 23:34:18 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,4 +7,6 @@ STR=$'g\nn\n1\n\n+512M\nt\n1\nn\n\n\n\nw'
 echo "$STR" | fdisk /dev/sda
 mkfs.vfat -F 32 /dev/sda1
 mkfs.btrfs /dev/sda2
+mount /dev/sda2 /mnt
+mount /dev/sda1 /mnt/boot
 pacstrap /mnt base linux linux-firmware base-devel vim
