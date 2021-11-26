fc406cf69ea3d66072337899a848f7d22c6886f8
swastyak <43711083+swastyak@users.noreply.github.com>
2021-11-26 23:36:29 +0000

    fixed mounting

--- script.sh
+++ script.sh
@@ -8,5 +8,6 @@ echo "$STR" | fdisk /dev/sda
 mkfs.vfat -F 32 /dev/sda1
 mkfs.btrfs /dev/sda2
 mount /dev/sda2 /mnt
+mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 pacstrap /mnt base linux linux-firmware base-devel vim
