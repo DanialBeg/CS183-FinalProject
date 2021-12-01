aab535f968349dc222f20957d2e4d90572f621dc
swastyak <43711083+swastyak@users.noreply.github.com>
2021-11-30 17:54:22 -0800

    MBR

--- script.sh
+++ script.sh
@@ -3,9 +3,11 @@ ls /sys/firmware/efi/efifvars
 ip link
 ping -c 3 archlinux.org
 timedatectl set-ntp true
-STR=$'g\nn\n1\n\n+512M\nt\n1\nn\n\n\n\nw'
+# STR=$'g\nn\n1\n\n+512M\nt\n1\nn\n\n\n\nw'
+STR=$'o\nn\n1\n\n+128M\na\nn\np\n2\n\n\nw'
 echo "$STR" | fdisk /dev/sda
-mkfs.vfat -F 32 /dev/sda1
+# mkfs.vfat -F 32 /dev/sda1
+mkfs.ext4 /dev/sda1
 mkfs.btrfs /dev/sda2
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
@@ -15,10 +17,10 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt << "EOT"
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
-echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+echo en_US.UTF-8 UTF-8 > /etc/locale.gen
 locale-gen
 touch /etc/hostname
-echo ucr_localhost >> /etc/hostname
+echo ucr_localhost > /etc/hostname
 pacman -S networkmanager grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
