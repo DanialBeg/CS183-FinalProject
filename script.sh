5f05460fd6db942dafb423d85a520d8c8c95c91a
swastyak <43711083+swastyak@users.noreply.github.com>
2021-11-30 17:23:27 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ mkfs.btrfs /dev/sda2
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
-pacstrap /mnt base linux linux-firmware base-devel vim
+pacstrap /mnt base linux linux-firmware base-devel vim networkmanager grub efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt << "EOT"
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
@@ -19,8 +19,6 @@ echo en_US.UTF-8 UTF-8 << /etc/locale.gen
 locale-gen
 touch /etc/hostname
 echo ucr_localhost << /etc/hostname
-mkinitcpio -P
-passwd
 pacman -S networkmanager grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
