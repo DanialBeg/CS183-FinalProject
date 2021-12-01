c84045f975c180c4ef1b1f7a2fefa0bb4e60ea38
swastyak <43711083+swastyak@users.noreply.github.com>
2021-11-30 17:33:21 -0800

    Update and rename chroot.sh to beginning.sh

---
+++ beginning.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ls /sys/firmware/efi/efifvars
+ip link
+ping -c 3 archlinux.org
+timedatectl set-ntp true
+STR=$'g\nn\n1\n\n+512M\nt\n1\nn\n\n\n\nw'
+echo "$STR" | fdisk /dev/sda
+mkfs.vfat -F 32 /dev/sda1
+mkfs.btrfs /dev/sda2
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+pacstrap /mnt base linux linux-firmware base-devel vim networkmanager grub efibootmgr
+genfstab -U /mnt >> /mnt/etc/fstab
