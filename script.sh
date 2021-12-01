98d884c26e0ded5e5c32c971503a92b5bbab6b1d
swastyak <43711083+swastyak@users.noreply.github.com>
2021-11-30 17:38:10 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,10 +15,10 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt << "EOT"
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
-echo en_US.UTF-8 UTF-8 << /etc/locale.gen
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
 touch /etc/hostname
-echo ucr_localhost << /etc/hostname
+echo ucr_localhost >> /etc/hostname
 pacman -S networkmanager grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
